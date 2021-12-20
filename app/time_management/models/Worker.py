9cf60fab48565c9e2002d42a8e4839d1f4bd1322
Ivan Ponomarev <ivan.ponomarev.pi@gmail.com>
2021-12-20 09:12:56 +0100

    More or less working code and polished (to a minimum extent)
     * One TODO is left unfinished

--- app/time_management/models/Worker.py
+++ app/time_management/models/Worker.py
@@ -2,9 +2,18 @@ from django.db import models
 
 
 class Worker(models.Model):
+	"""
+		Represents Worker
+
+		name - Full worker name
+		birthdate - Birthdate of the worker for better identification
+		max_shifts_per_week - Max amount of shifts per 1 week for this worker. Applicable only when
+			settings.TIME_MANAGEMENT_APP['LIMIT_WEEKLY_SHIFTS'] is True and max_shifts_per_week is not empty.
+	"""
+
 	name = models.CharField(max_length=255)
 	birthdate = models.DateField(null=True)
-	max_shifts_per_week = models.IntegerField(null=True)
+	max_shifts_per_week = models.IntegerField(null=True, default=4)
 
 	def __str__(self):
 		return f"[ {self.birthdate} ] {self.name}"
--- app/time_management/tests.py
+++ app/time_management/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+from time_management.models import Worker
+
+
+class SimpleTestCase(TestCase):
+
+	def setUp(self) -> None:
+		Worker.objects.create(name="Test Doe", birthdate='2021-02-02')
+
+	def test_worker_getting(self):
+		worker = Worker.objects.get(name="Test Doe")
+		self.assertIsNotNone(worker)
+		sample = f"[ 2021-02-02 ] Test Doe"
+		self.assertEqual(str(worker), sample)
+
+	def test_shifts_creation_for_worker(self):
+		worker = Worker.objects.get(name="Test Doe")
+
+		range_iter = range(1, 5)
+
+		for i in range_iter:
+			worker.shifts.create(target_dt=f'2021-12-0{i} 00:00', memo=f'TEST{i}')
+
+		self.assertEqual(len(range_iter), worker.shifts.count())
